ec6f1c926682c1833d8dbfefc3b6738bb0e793f1
Leticia Eto Filó Candido <160032869+Leticia-Eto@users.noreply.github.com>
2025-09-09 08:00:00 -0300

    Update Contents.swift

--- HofsLeticia.playground/Contents.swift
+++ HofsLeticia.playground/Contents.swift
@@ -10,4 +10,16 @@ var doubledList = numList.map{
     $0*2
 }
 
+var oddList = numList.filter {
+    num in
+    return (num.truncatingRemainder(dividingBy: 2)) != 0
+}
+var shortOddList = numList.filter {
+    ($0.truncatingRemainder(dividingBy: 2)) != 0
+}
+print(shortOddList)//o print é para o user ver embaixo
 
+var menusList = numList.filter {
+    num in
+    return (num) < 0
+}
